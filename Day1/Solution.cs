37cbd4c4aa174ec3559fc3ea0cf3323b6ec28e50
MiloszSobczyk <m.sobczyk.official@gmail.com>
2023-12-12 14:16:16 +0100

    Add day 1 part 2 solution

--- Day1/Solution.cs
+++ Day1/Solution.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,11 +10,25 @@ namespace Day1
     internal class Solution
     {
         public String FilePath { get; private set; } //input filepath related to .exe file
+        private Dictionary<string, int> stringDigits;
         public Solution(string filePath = $"../../../input.txt")
         {
             FilePath = filePath;
+            stringDigits = new Dictionary<string, int>()
+            {
+                { "one",   1 },
+                { "two",   2 },
+                { "three", 3 },
+                { "four",  4 },
+                { "five",  5 },
+                { "six",   6 },
+                { "seven", 7 },
+                { "eight", 8 },
+                { "nine",  9 },
+            };
+
         }
-        private int Solve()
+        private int SolvePart1()
         {
             if (!File.Exists(FilePath)) 
                 return -1;
@@ -38,12 +53,69 @@ namespace Day1
                 }
                 return sum;
             }
-
+        }
+        private int SolvePart2()
+        {
+            if (!File.Exists(FilePath))
+                return -1;
+            using (StreamReader sr = new StreamReader(FilePath))
+            {
+                int sum = 0;
+                while (sr.Peek() >= 0)
+                {
+                    string line = sr.ReadLine()!;
+                    int firstDigit = -1;
+                    int lastDigit = -1;
+                    int firstInd = line.Length - 1;
+                    int lastInd = 0;
+                    foreach(KeyValuePair<string, int> kvp in stringDigits)
+                    {
+                        int ind = line.IndexOf(kvp.Key);
+                        if(ind != -1)
+                        {
+                            if(ind < firstInd)
+                            {
+                                firstInd = ind;
+                                firstDigit = kvp.Value;
+                            }
+                            ind = line.LastIndexOf(kvp.Key);
+                            if(ind > lastInd)
+                            {
+                                lastInd = ind;
+                                lastDigit = kvp.Value;
+                            }
+                        }
+                    }
+                    for(int i = 0; i <= firstInd; ++i)
+                    {
+                        char c = line[i];
+                        if (c >= '0' && c <= '9')
+                        {
+                            firstInd = i;
+                            firstDigit = c - '0';
+                            break;
+                        }
+                    }
+                    for (int i = line.Length - 1; i >= lastInd; --i)
+                    {
+                        char c = line[i];
+                        if (c >= '0' && c <= '9')
+                        {
+                            lastInd = i;
+                            lastDigit = c - '0';
+                            break;
+                        }
+                    }
+                    sum += firstDigit * 10 + lastDigit;
+                }
+                return sum;
+            }
         }
         public static void Main()
         {
             Solution solution = new Solution();
-            Console.WriteLine(solution.Solve());
+            Console.WriteLine(solution.SolvePart1());
+            Console.WriteLine(solution.SolvePart2());
         }
     }
 }
---
+++ Day3/Solution.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    internal class Solution
+    {
+        public String FilePath { get; private set; } //input filepath related to .exe file
+        private Dictionary<string, int> colorCounts;
+        public Solution(string filePath = $"../../../input.txt")
+        {
+            FilePath = filePath;
+            colorCounts = new Dictionary<string, int>()
+            {
+                { "red", 12 },
+                { "green", 13 },
+                { "blue", 14 }
+            };
+        }
+        private int Solve()
+        {
+            if (!File.Exists(FilePath))
+                return -1;
+            using (StreamReader sr = new StreamReader(FilePath))
+            {
+
+            }
+            return 0;
+        }
+        private int ProcessLine(string linePrev, string line, string lineNext)
+        {
+            int sum = 0;
+            int number = 0;
+            bool inNumber = false;
+            bool include = false;
+            for(int i = 0; i < line.Length; ++i)
+            {
+                inNumber = Char.IsNumber(line[i]);
+                //include |= (linePrev != null && linePrev[i])
+                if(inNumber)
+                {
+                    number = number * 10 + line[i];
+                    
+                }
+            }
+            return sum;
+        }
+
+        public static void Main()
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.Solve());
+        }
+    }
+}
